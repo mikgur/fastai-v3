4e3add5907b710008c45463888ba471ce05d4e7f
mikgur <gurevich3004@gmail.com>
2019-05-25 07:42:54 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/2vkeu6b3rkc94nv/avengers_classificator.pkl?dl=1'
+export_file_name = 'avengers_classificator.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
+classes = ['ironman','hulk','captain_america', 'spiderman', 'thor', 'black_widow']
 path = Path(__file__).parent
 
 app = Starlette()
